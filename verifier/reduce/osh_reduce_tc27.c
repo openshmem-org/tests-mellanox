47eb72a075603d3c46e45eeb1d1fd6db476c09a9
Mikhail Brinskiy <brminich@users.noreply.github.com>
2021-11-11 09:59:16 +0100

    REDUCE: Fix overflow in short_sum tests (#54)

--- verifier/reduce/osh_reduce_tc27.c
+++ verifier/reduce/osh_reduce_tc27.c
@@ -7,6 +7,8 @@
  *
  * $HEADER$
  */
+#include <limits.h>
+
 #include "osh_def.h"
 #include "osh_cmn.h"
 #include "osh_log.h"
@@ -75,6 +77,39 @@ static const AOPT_DESC  self_opt_desc[] =
     },
 	{ 0, AOPT_NOARG, aopt_set_literal( 0 ), aopt_set_string( NULL ), NULL }
 };
+
+
+
+/*
+ * Solve quadratic equation to find the closest value of n for the given sum
+ * of first n numbers, according to the formulas:
+ * sum = n * (n + 1) / 2 - for the sum of all numbers
+ * sum = n * (n + 2) / 4 - for the sum of even numbers
+ */
+static int osh_reduce_max_n(TYPE_VALUE sum, int count_even)
+{
+    int a = 1;
+    int b, c;
+    double discriminant;
+
+    if (count_even)
+    {
+        b = 2;
+        c = -4 * sum;
+    }
+    else
+    {
+        b = 1;
+        c = -2 * sum;
+    }
+
+    discriminant = (b*b) - (4*a*c);
+    if (discriminant < 0) {
+        return 0;
+    }
+
+    return (-b + sqrt(discriminant)) / (2*a);
+}
 
 
 /****************************************************************************
@@ -274,10 +309,18 @@ static int test_item2(void)
     TYPE_VALUE expect_value = 0;
     int num_proc = 0;
     int my_proc = 0;
+    int max_proc_num = 0;
 
     num_proc = _num_pes();
     my_proc = _my_pe();
+    max_proc_num = osh_reduce_max_n(SHRT_MAX, 0);
 
+   /* This test suite checks that the total sum calculated by shmem_short_sum_to_all
+    * will not be bigger than SHRT_MAX value. If the number of processes is big
+    * enough to oveflow maximal short type, calculate the maximal possible rank id
+    * and use 0 for all processes with rank id bigger that that value.
+    */
+    if (my_proc > max_proc_num) my_proc = 0;
 
     pWrk = shmalloc(sizeof(*pWrk) * sys_max(1/2 + 1, _SHMEM_REDUCE_MIN_WRKDATA_SIZE));
     if (pWrk)
@@ -290,6 +333,7 @@ static int test_item2(void)
     {
         TYPE_VALUE value = DEFAULT_VALUE;
         int j = 0;
+        int k;
 
         /* Set initial target value */
         *target_addr = DEFAULT_VALUE;
@@ -299,10 +343,8 @@ static int test_item2(void)
         *source_addr = source_value;
 
         /* Define expected value */
-        expect_value = 0;
-        {
-            int k = num_proc;
-            while (k--) expect_value += k;
+        for (k = sys_min(max_proc_num, num_proc - 1), expect_value = 0; k > 0; --k) {
+            expect_value += k;
         }
 
         /* This guarantees that PE set initial value before peer change one */
@@ -449,10 +491,18 @@ static int test_item4(void)
     TYPE_VALUE expect_value = 0;
     int num_proc = 0;
     int my_proc = 0;
+    int max_proc_num = 0;
 
     num_proc = _num_pes();
     my_proc = _my_pe();
+    max_proc_num = osh_reduce_max_n(SHRT_MAX, 0);
 
+   /* This test suite checks that the total sum calculated by shmem_short_sum_to_all
+    * will not be bigger than SHRT_MAX value. If the number of processes is big
+    * enough to oveflow maximal short type, calculate the maximal possible rank id
+    * and use 0 for all processes with rank id bigger that that value.
+    */
+    if (my_proc > max_proc_num) my_proc = 0;
 
     pWrk = shmalloc(sizeof(*pWrk) * sys_max(1/2 + 1, _SHMEM_REDUCE_MIN_WRKDATA_SIZE));
     if (pWrk)
@@ -473,8 +523,8 @@ static int test_item4(void)
         /* Define expected value */
         expect_value = 0;
         {
-            int k = num_proc;
-            while (k--) expect_value += k;
+            int k = sys_min(max_proc_num, num_proc - 1);
+            while (k) expect_value += k--;
         }
 
         /* This guarantees that PE set initial value before peer change one */
@@ -644,9 +694,18 @@ static int test_item6(void)
     TYPE_VALUE expect_value = 0;
     int num_proc = 0;
     int my_proc = 0;
+    int max_proc_num = 0;
 
     num_proc = _num_pes();
     my_proc = _my_pe();
+    max_proc_num = osh_reduce_max_n(SHRT_MAX, 0);
+
+   /* This test suite checks that the total sum calculated by shmem_short_sum_to_all
+    * will not be bigger than SHRT_MAX value. If the number of processes is big
+    * enough to oveflow maximal short type, calculate the maximal possible rank id
+    * and use 0 for all processes with rank id bigger that that value.
+    */
+    if (my_proc > max_proc_num) my_proc = 0;
 
     target_addr = (TYPE_VALUE*)shmalloc(sizeof(*target_addr) * __max_buffer_size);
     source_addr = (TYPE_VALUE*)shmalloc(sizeof(*source_addr) * __max_buffer_size);
@@ -677,8 +736,8 @@ static int test_item6(void)
                 /* Define expected value */
                 expect_value = 0;
                 {
-                    int k = num_proc;
-                    while (k--) expect_value += k;
+                    int k = sys_min(max_proc_num, num_proc - 1);
+                    while (k) expect_value += k--;
                 }
 
                 /* This guarantees that PE set initial value before peer change one */
@@ -758,9 +817,18 @@ static int test_item7(void)
     TYPE_VALUE expect_value = 0;
     int num_proc = 0;
     int my_proc = 0;
+    int max_proc_num = 0;
 
     num_proc = _num_pes();
     my_proc = _my_pe();
+    max_proc_num = osh_reduce_max_n(SHRT_MAX, 1);
+
+   /* This test suite checks that the total sum calculated by shmem_short_sum_to_all
+    * will not be bigger than SHRT_MAX value. If the number of processes is big
+    * enough to oveflow maximal short type, calculate the maximal possible rank id
+    * and use 0 for all processes with rank id bigger that that value.
+    */
+    if (my_proc > max_proc_num) my_proc = 0;
 
     target_addr = (TYPE_VALUE*)shmalloc(sizeof(*target_addr) * __max_buffer_size);
     source_addr = (TYPE_VALUE*)shmalloc(sizeof(*source_addr) * __max_buffer_size);
@@ -793,8 +861,12 @@ static int test_item7(void)
                 if (my_proc % 2)    expect_value = DEFAULT_VALUE;
                 else
                 {
-                    int k = num_proc;
-                    while (k--) expect_value += (k % 2 ? 0 : k);
+                    int k = sys_min(max_proc_num, num_proc - 1);
+                    while (k)
+                    {
+                        expect_value += (k % 2 ? 0 : k);
+                        --k;
+                    }
                 }
 
                 /* This guarantees that PE set initial value before peer change one */
@@ -879,6 +951,7 @@ static int test_item8(void)
     TYPE_VALUE expect_value = 0;
     int num_proc = 0;
     int my_proc = 0;
+    int max_proc_num = 0;
     long* pSyncMult = NULL;
     TYPE_VALUE* pWrkMult = NULL;
     int pSyncNum = 2;
@@ -886,6 +959,14 @@ static int test_item8(void)
 
     num_proc = _num_pes();
     my_proc = _my_pe();
+    max_proc_num = osh_reduce_max_n(SHRT_MAX, 1);
+
+   /* This test suite checks that the total sum calculated by shmem_short_sum_to_all
+    * will not be bigger than SHRT_MAX value. If the number of processes is big
+    * enough to oveflow maximal short type, calculate the maximal possible rank id
+    * and use 0 for all processes with rank id bigger that that value.
+    */
+    if (my_proc > max_proc_num) my_proc = 0;
 
     pSyncMult = shmalloc(sizeof(*pSyncMult) * pSyncNum * _SHMEM_REDUCE_SYNC_SIZE);
     if (pSyncMult)
@@ -925,8 +1006,12 @@ static int test_item8(void)
                 if (my_proc % 2)    expect_value = DEFAULT_VALUE;
                 else
                 {
-                    int k = num_proc;
-                    while (k--) expect_value += (k % 2 ? 0 : k);
+                    int k = sys_min(max_proc_num, num_proc - 1);
+                    while (k)
+                    {
+                        expect_value += (k % 2) ? 0 : k;
+                        --k;
+                    }
                 }
 
                 int in_active_set = check_within_active_set(0, 1, ((num_proc / 2) + (num_proc % 2)), my_proc, num_proc);
